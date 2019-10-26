e7732b27e4fc390df8b9ec11f91ebf1bdc2c7f5f
xserrat <xavier.serrat22@gmail.com>
2019-10-26 15:02:14 +0200

    Add first approach to change the color of the matrix using a color picker and websockets

--- server.js
+++ server.js
@@ -1,21 +1,42 @@
 var express = require('express');
+var http = require('http');
+var io = require('socket.io');
+
 var app = express();
+
+var sense_hat = require('node-sense-hat');
+matrix = sense_hat.Leds;
 
 // Enable HTML template middleware
 app.engine('html', require('ejs').renderFile);
 
 // Enable static CSS styles
-app.use(express.static('styles'));
+app.use(express.static('public'));
 
 // reply to request with "Hello World!"
 app.get('/', function (req, res) {
   res.render('index.html');
 });
 
-//start a server on port 80 and log its start to our console
-var server = app.listen(80, function () {
+var server = http.createServer(app);
 
+app.use(express.static(__dirname + '/static'))
+server.listen(80, function() {
   var port = server.address().port;
   console.log('Example app listening on port ', port);
-
 });
+
+io = io.listen(server);
+
+io.on('connection', function(socket) {
+  socket.emit('server_connected', {
+    "message": "Socket connected!"
+  });
+
+  socket.on('change_color', function (data) {
+    let color = data.color_code;
+    let rounded_color = [Math.round(color[0]), Math.round(color[1]), Math.round(color[2])];
+    matrix.clear(rounded_color);
+  });
+});
+
